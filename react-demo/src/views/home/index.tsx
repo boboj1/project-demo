8e06cd4f1137422ade2a1e120cba9ed249ac7292
boboj1 <2752719361@qq.com>
2025-08-28 15:04:43 +0800

    react-demo+

--- react-demo/src/views/home/index.tsx
+++ react-demo/src/views/home/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect, useRef, useContext } from 'react'
+import React, { useState, useCallback, useEffect, useRef, useContext } from 'react'
 import { debounce } from 'lodash-es'
 import { Button } from 'antd'
 import { useDebounceFn } from '@/hooks'
@@ -10,31 +10,14 @@ export default function Home() {
 	console.log('theme', theme)
 	console.log('home render')
 
-	return (
-		<div>
-			<Button onClick={() => setStatus(s => !s)}>Toggle</Button>
-			{status && <List />}
-		</div>
-	)
-}
-
-function List() {
-	const [count, setCount] = useState(0)
-	const [num, setNum] = useState(0)
-
-	console.log('list render')
-
-	const handleClick = useDebounceFn(() => {
-		setCount(c => c + 1)
-		console.log('handleClick')
-	}, 1000)
+	const clickHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
+		console.log(e)
+		console.log('Button clicked')
+	}
 
 	return (
 		<div>
-			<span>{count}</span>
-			<Button type="primary" onClick={handleClick}>
-				点击
-			</Button>
+			<button onClick={clickHandler}>click</button>
 		</div>
 	)
 }
