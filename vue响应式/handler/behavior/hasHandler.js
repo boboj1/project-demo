2734f678c464adc7dcfb7541bfebc8a44fc0478c
boboj1 <2752719361@qq.com>
2025-09-04 11:26:38 +0800

    新增vue源码学习demo

--- vue响应式/handler/behavior/hasHandler.js
+++
@@ -1,7 +0,0 @@
-import track from '../../effect/track.js'
-import { TrackOpTypes } from '../../utils/index.js'
-
-export default function (target, key) {
-  track(target, TrackOpTypes.HAS, key)
-  return Reflect.has(target, key)
-}
--- vue响应式/handler/behavior/ownKeysHandler.js
+++
@@ -1,7 +0,0 @@
-import track from '../../effect/track.js'
-import { TrackOpTypes } from '../../utils/index.js'
-
-export default function (target) {
-  track(target, TrackOpTypes.ITERATE)
-  return Reflect.ownKeys(target)
-}
